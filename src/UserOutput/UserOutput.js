f06ef092c18b56bfe87838311640fa036d4559fe
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-05-10 15:54:54 -0600

    working on initial components

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const UserOutput = (props) => {
+    return (
+        <div>
+            <p onClick={props.click}>
+            {props.username}, Eat the fat cats food i show my fluffy belly but it's a trap! if you pet it i will tear up your hand. 
+            Leave fur on owners clothes use lap as chair x meow all night need to check on human, have not seen in an hour might be dead oh look, 
+            human is alive, hiss at human, feed me but annoy owner until he gives you food say meow repeatedly until belly rubs, feels good so 
+            decide to want nothing to do with my owner today. 
+            </p>
+            <p>
+            I'm getting hungry. Chill on the couch table. Proudly present butt to human kitty kitty so murf pratt ungow 
+            ungow paw your face to wake you up in the morning or love cats making all the muffins spread kitty litter all over house. 
+            Asdflkjaertvlkjasntvkjn (sits on keyboard) that box? i can fit in that box. X when in doubt, wash for cuddle no cuddle cuddle love 
+            scratch scratch so i am the best. 
+            </p>
+        </div>
+    )
+}
+
+export default UserOutput;
